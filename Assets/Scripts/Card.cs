5aba50715635e663bc14e6d874e8f832b8109d69
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2020-09-19 22:16:49 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Card : MonoBehaviour
+{
+    public cardColors color = cardColors.Red;
+    public int number = 8;
+
+    [Header("Assign Values")]
+    public TextMeshProUGUI mainNum;
+    public TextMeshProUGUI minorNumUp;
+    public TextMeshProUGUI minorNumDown;
+
+    [Header("Assign Materials")]
+    public Material redMaterialColor;
+    public Material blueMaterialColor;
+    public Material yellowMaterialColor;
+    public Material greenMaterialColor;
+
+
+    MeshRenderer mr;
+    void Start()
+    {
+
+        mr = GetComponentInChildren<MeshRenderer>();
+        UpdateColor();
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        mainNum.text = number.ToString();
+        minorNumUp.text = number.ToString();
+        minorNumDown.text = number.ToString();
+    }
+
+    void UpdateColor()
+    {
+        switch (color)
+        {
+            case cardColors.Red:
+                mr.material = redMaterialColor;
+                break;
+            case cardColors.Green:
+                mr.material = greenMaterialColor;
+                break;
+            case cardColors.Blue:
+                mr.material = blueMaterialColor;
+                break;
+            case cardColors.Yellow:
+                mr.material = yellowMaterialColor;
+                break;
+        }
+
+    }
+}
+
+public enum cardColors { Red, Green, Blue, Yellow  }
